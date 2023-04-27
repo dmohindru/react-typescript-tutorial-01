4f82caaca375995a24f9f761152e73f738d066aa
Dhruv Mohindru <dmohindru@gmail.com>
2023-04-28 07:12:24 +1000

    Finished section buiding this sample app

--- README.md
+++ README.md
@@ -2,5 +2,10 @@
 This app demonstrates how to use typescript with react and redux.
 # Overview
 [App overview](https://www.udemy.com/course/typescript-the-complete-developers-guide/learn/lecture/15067226#content)
+# Notes
+- generate a react app with typescript support built in
+```shell
+npx create-react-app <appname> --template typescript
+```
 
 
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,39 +1,68 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Todo, fetchTodos } from '../actions';
+import { Todo, fetchTodos, deleteTodo } from '../actions';
 import { StoreState } from '../reducers';
 
 interface AppProps {
-    todos: Todo[];
-    fetchTodos(): any;
+  todos: Todo[];
+  fetchTodos: Function;
+  deleteTodo: typeof deleteTodo;
 }
 
-
-
-class _App extends React.Component<AppProps> {
-    
-    onButtonClick = (): void => {
-        this.props.fetchTodos();
-    }
-
-    renderList(): JSX.Element[] {
-        return this.props.todos.map((todo: Todo) => {
-            return <div key={todo.id}>{todo.title}</div>
-        });
-    }
-
-    render() {    
-        return ( 
-            <div>
-                <button onClick={this.onButtonClick}>Fetch</button>
-                {this.renderList()}
-            </div>
-        );
-    }
+interface AppState {
+  fetching: boolean;
 }
 
-const mapStateToProps = ({ todos }: StoreState): {todos: Todo[]} => {
-    return { todos };
+// First generic argument is for app properties
+// Second generic argument is for app state
+class _App extends React.Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+
+    this.state = { fetching: false };
+  }
+
+  componentDidUpdate(prevProps: AppProps): void {
+    if (!prevProps.todos.length && this.props.todos.length) {
+      this.setState({ fetching: false });
+    }
+  }
+
+  onButtonClick = (): void => {
+    this.props.fetchTodos();
+    this.setState({ fetching: true });
+  };
+
+  onTodoClick = (id: number): void => {
+    this.props.deleteTodo(id);
+  };
+
+  renderList(): JSX.Element[] {
+    return this.props.todos.map((todo: Todo) => {
+      return (
+        <div onClick={() => this.onTodoClick(todo.id)} key={todo.id}>
+          {todo.title}
+        </div>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.onButtonClick}>Fetch</button>
+        {this.state.fetching ? 'LOADING' : null}
+        {this.renderList()}
+      </div>
+    );
+  }
 }
 
-export const App = connect(mapStateToProps, { fetchTodos })(_App);
+const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
+  return { todos };
+};
+
+export const App = connect(
+  mapStateToProps,
+  { fetchTodos, deleteTodo }
+)(_App);
